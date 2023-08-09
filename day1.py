77bb50436a212f9f8d148916a06574ce2c5e06db
Michelle Song <101286675+em-ess@users.noreply.github.com>
2023-08-09 10:15:30 -0500

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,1 +1,24 @@
+# PROBLEM 1 FOR THE DAY MON JUL 31
 
+# 2236. Root Equals Sum of Children
+# You are given the root of a binary tree that consists of exactly 3 nodes: the root, its left child, and its right child.
+#
+# Return true if the value of the root is equal to the sum of the values of its two children, or false otherwise.
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution(object):
+    def checkTree(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: bool
+        """
+       return root.val == (root.left.val + root.right.val)
+#Success Runtime: 
+  
